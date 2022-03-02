20972e254938832eb61d9e762893f75fef58f5e6
olivialin21 <olivia891031@gmail.com>
2022-03-02 21:48:28 +0800

    rewrite json

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+      <View style={styles.headerStyle}>
+        <Text style={styles.textStyle}>MAYDAY'S ALBUMS</Text>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    // Android Only
+    elevation: 4
+  },
+  textStyle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+});
+
+export default Header;
